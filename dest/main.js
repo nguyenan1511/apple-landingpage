9499f3a4440695d1c1d38bcc67668aea91068fea
nguyenan1511 <nguyenan151119@gmail.com>
2025-08-12 15:34:31 +0700

    feat: mobile homepage

--- dest/main.js
+++ dest/main.js
@@ -152,51 +152,6 @@ $(document).ready(function () {
   }
   clickSubMenu();
 
-  // HOMEPAGE
-  function sliderHero() {
-    if ($(".scherohome .slider__inner").length) {
-      let $slider = $(".scherohome .slider__inner"),
-        $sliderParent = $slider.parent(),
-        opt = {
-          cellAlign: "center",
-          initialIndex: 2,
-          pageDots: true,
-          autoPlay: false,
-          draggable: ">1",
-          pauseAutoPlayOnHover: false,
-          dragThreshold: 0,
-          prevNextButtons: false,
-          selectedAttraction: 0.008,
-          friction: 0.168,
-          wrapAround: true,
-          // autoPlay: 4500,
-        };
-      $slider.flickity(opt);
-
-      // parallax
-      var flkty = $slider.data("flickity");
-      var $imgs = $(".scherohome .slider__inner .slide img");
-
-      $slider.on("scroll.flickity", function (event, progress) {
-        flkty.slides.forEach(function (slide, i) {
-          var img = $imgs[i];
-          var x = ((slide.target + flkty.x) * -1) / 1.5;
-          img.style.transform = "translateX( " + x + "px)";
-        });
-      });
-
-      // previous | next
-      let $prevBtn = $(".btncontrols.--prev", $sliderParent),
-        $nextBtn = $(".btncontrols.--next", $sliderParent);
-      $prevBtn.on("click", function () {
-        $slider.flickity("previous", true);
-      });
-      $nextBtn.on("click", function () {
-        $slider.flickity("next", true);
-      });
-    }
-  }
-
   // TV+ Slider
   function tvSlider() {
     if ($(".tv-slider__wrapper").length) {
@@ -304,86 +259,6 @@ $(document).ready(function () {
     }
   }
 
-  function sliderHeroProjectDetail() {
-    if ($(".scprojectdetail .slider__inner").length) {
-      let $slider = $(".scprojectdetail .slider__inner"),
-        $sliderParent = $slider.parent();
-      opt = {
-        cellAlign: "center",
-        pageDots: true,
-        autoPlay: false,
-        draggable: ">1",
-        pauseAutoPlayOnHover: false,
-        dragThreshold: 0,
-        prevNextButtons: false,
-        selectedAttraction: 0.008,
-        friction: 0.168,
-        fullscreen: true,
-        wrapAround: true,
-      };
-      $slider.flickity(opt);
-
-      // GET FULLSCREEN
-      $slider.on("fullscreenChange.flickity", function (event, isFullscreen) {
-        if (isFullscreen) {
-          let $sliderFullscreen = $(".--afterFull .container");
-          $sliderFullscreen.after("").appendTo(".sliderfullscreen");
-        } else {
-          let $sliderFullscreenAfter = $(".sliderfullscreen .container");
-          $sliderFullscreenAfter.after("").appendTo(".--afterFull");
-          $slider.flickity("resize");
-        }
-      });
-
-      // previous | next
-      let $prevBtn = $(".btncontrols.--prev", $sliderParent),
-        $nextBtn = $(".btncontrols.--next", $sliderParent);
-      $prevBtn.on("click", function () {
-        $slider.flickity("previous", true);
-      });
-      $nextBtn.on("click", function () {
-        $slider.flickity("next", true);
-      });
-    }
-  }
-
-  // HOVER SERVICES
-  function hoverServices() {
-    let item = $(".scserviceshome__list .item"),
-      itemContent = $(".scserviceshome__content .content");
-    item.hover(
-      function () {
-        let index = $(this).index();
-        $(this).removeClass("--dim").siblings().addClass("--dim");
-        itemContent
-          .eq(index)
-          .addClass("active")
-          .siblings()
-          .removeClass("active");
-      },
-      function () {
-        item.removeClass("--dim");
-        itemContent.eq(1).addClass("active").siblings().removeClass("active");
-      }
-    );
-  }
-  hoverServices();
-
-  // SET HEIGHT SERVICE ITEMS
-  function setHeightServieItems() {
-    if ($(".servicepage").length) {
-      let serviceItems = $(".service__item");
-      setTimeout(() => {
-        serviceItems.each(function (i, item) {
-          let h = $(item).outerHeight();
-          $(item).css({
-            height: h + "px",
-          });
-        });
-      }, 200);
-    }
-  }
-
   function setHeightAutoServieItems() {
     if ($(".servicepage").length) {
       $(".service__item").css({
@@ -436,67 +311,6 @@ $(document).ready(function () {
     }
   }
 
-  // SLIDER PROJECTS
-  function sliderProjects() {
-    if ($(".scprojects__slider").length) {
-      let $slider = $(".scprojects__slider"),
-        $sliderParent = $slider.parent(),
-        opt = {
-          cellAlign: "center",
-          dragThreshold: 0,
-          prevNextButtons: false,
-          imagesLoaded: true,
-          lazyLoad: 4,
-          wrapAround: true,
-          freeScroll: true,
-          pageDots: false,
-        };
-      $slider.flickity(opt);
-
-      // previous | next
-      let $prevBtn = $(".btncontrols.--prev", $sliderParent),
-        $nextBtn = $(".btncontrols.--next", $sliderParent);
-      $prevBtn.on("click", function () {
-        $slider.flickity("previous", true);
-      });
-      $nextBtn.on("click", function () {
-        $slider.flickity("next", true);
-      });
-      $slider.on("change.flickity", function (slide, index) {});
-    }
-  }
-
-  // SLIDER NEWS
-  function sliderNews() {
-    if ($(".scnews__slider").length) {
-      let $slider = $(".scnews__slider"),
-        $sliderParent = $slider.parent(),
-        opt = {
-          cellAlign: "left",
-          pageDots: false,
-          pauseAutoPlayOnHover: true,
-          dragThreshold: 10,
-          freeScroll: true,
-          prevNextButtons: false,
-          contain: true,
-          selectedAttraction: 0.02,
-          imagesLoaded: true,
-        };
-      $slider.flickity(opt);
-
-      // previous | next
-      let $prevBtn = $(".btncontrols.--prev", $sliderParent),
-        $nextBtn = $(".btncontrols.--next", $sliderParent);
-      $prevBtn.on("click", function () {
-        $slider.flickity("previous", true);
-      });
-      $nextBtn.on("click", function () {
-        $slider.flickity("next", true);
-      });
-      $slider.on("change.flickity", function (slide, index) {});
-    }
-  }
-
   // BACK TO TOP
   function backToTop() {
     const btn = $(".backtotop");
@@ -507,7 +321,6 @@ $(document).ready(function () {
   }
 
   $(window).resize(function () {
-    setHeightAutoServieItems();
     setTimeout(function () {
       reloadOnResize();
       locoScroll.update();
@@ -535,186 +348,6 @@ $(document).ready(function () {
     }
   }
 
-  function handleGallerySlider() {
-    const slider = $(".scgallery__slider");
-    if (!slider?.length) return;
-
-    slider?.flickity({
-      cellAlign: "center",
-      pageDots: false,
-      dragThreshold: 10,
-      freeScroll: true,
-      prevNextButtons: false,
-      contain: true,
-      selectedAttraction: 0.02,
-      imagesLoaded: true,
-      wrapAround: true,
-    });
-  }
-
-  // IMAGE TRAIL EFFECT
-  const body = document.body;
-
-  // helper functions
-  const MathUtils = {
-    lerp: (a, b, n) => (1 - n) * a + n * b,
-    distance: (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1),
-  };
-
-  // get the mouse position
-  const getMousePos = (ev) => {
-    let posx = 0;
-    let posy = 0;
-    if (!ev) ev = window.event;
-    if (ev.pageX || ev.pageY) {
-      posx = ev.pageX;
-      posy = ev.pageY;
-    } else if (ev.clientX || ev.clientY) {
-      posx = ev.clientX + body.scrollLeft + docEl.scrollLeft;
-      posy = ev.clientY + body.scrollTop + docEl.scrollTop;
-    }
-    return { x: posx, y: posy };
-  };
-
-  let mousePos = (lastMousePos = cacheMousePos = { x: 0, y: 0 });
-
-  // update the mouse position
-  window.addEventListener("mousemove", (ev) => (mousePos = getMousePos(ev)));
-
-  const getMouseDistance = () =>
-    MathUtils.distance(mousePos.x, mousePos.y, lastMousePos.x, lastMousePos.y);
-
-  class Image {
-    constructor(el) {
-      this.DOM = { el: el };
-      this.defaultStyle = {
-        scale: 1,
-        x: 0,
-        y: 0,
-        opacity: 0,
-      };
-      this.getRect();
-    }
-
-    getRect() {
-      this.rect = this.DOM.el.getBoundingClientRect();
-    }
-    isActive() {
-      return gsap.isTweening(this.DOM.el) || this.DOM.el.style.opacity != 0;
-    }
-  }
-
-  class ImageTrail {
-    constructor() {
-      this.DOM = { content: document.querySelector(".trailimg") };
-      this.images = [];
-      [...this.DOM.content.querySelectorAll("img")].forEach((img) =>
-        this.images.push(new Image(img))
-      );
-      this.imagesTotal = this.images.length;
-      this.imgPosition = 0;
-      this.zIndexVal = 1;
-      this.threshold = 100;
-      requestAnimationFrame(() => this.render());
-    }
-    render() {
-      let distance = getMouseDistance();
-      cacheMousePos.x = MathUtils.lerp(
-        cacheMousePos.x || mousePos.x,
-        mousePos.x,
-        0.1
-      );
-      cacheMousePos.y = MathUtils.lerp(
-        cacheMousePos.y || mousePos.y,
-        mousePos.y,
-        0.1
-      );
-
-      if (distance > this.threshold) {
-        this.showNextImage();
-
-        ++this.zIndexVal;
-        this.imgPosition =
-          this.imgPosition < this.imagesTotal - 1 ? this.imgPosition + 1 : 0;
-
-        lastMousePos = mousePos;
-      }
-
-      let isIdle = true;
-      for (let img of this.images) {
-        if (img.isActive()) {
-          isIdle = false;
-          break;
-        }
-      }
-      if (isIdle && this.zIndexVal !== 1) {
-        this.zIndexVal = 1;
-      }
-
-      requestAnimationFrame(() => this.render());
-    }
-    showNextImage() {
-      const img = this.images[this.imgPosition];
-      gsap.killTweensOf(img.DOM.el);
-
-      new TimelineMax()
-        .set(
-          img.DOM.el,
-          {
-            startAt: { opacity: 0, scale: 1 },
-            opacity: 1,
-            scale: 1,
-            zIndex: this.zIndexVal,
-            x: cacheMousePos.x - img.rect.width / 2,
-            y: cacheMousePos.y - img.rect.height / 2,
-          },
-          0
-        )
-        .to(
-          img.DOM.el,
-          0.9,
-          {
-            ease: Expo.easeOut,
-            x: mousePos.x - img.rect.width / 2,
-            y: mousePos.y - img.rect.height / 2,
-          },
-          0
-        )
-        .to(
-          img.DOM.el,
-          1,
-          {
-            ease: Power1.easeOut,
-            opacity: 0,
-          },
-          0.4
-        )
-        .to(
-          img.DOM.el,
-          1,
-          {
-            ease: Quint.easeOut,
-            scale: 0.2,
-          },
-          0.4
-        );
-    }
-  }
-
-  // preload images
-  const preloadImages = () => {
-    return new Promise((resolve, reject) => {
-      imagesLoaded(document.querySelectorAll(".content__img"), resolve);
-    });
-  };
-
-  preloadImages().then(() => {
-    const $isTrail = document.querySelector(".trailimg");
-    if ($isTrail) {
-      new ImageTrail();
-    }
-  });
-
   // Improved seamless infinite scroll for media-slider-section using GSAP
   function autoScrollMediaSlider() {
     const wrapper = document.querySelector(".media-slider-wrapper");
@@ -723,20 +356,41 @@ $(document).ready(function () {
     // Only duplicate once
     if (!wrapper.dataset.duplicated) {
       const items = Array.from(wrapper.children);
-      items.forEach(item => {
+      items.forEach((item) => {
         const clone = item.cloneNode(true);
-        clone.classList.add('media-slider-item--clone');
+        clone.classList.add("media-slider-item--clone");
         wrapper.appendChild(clone);
       });
-      wrapper.dataset.duplicated = 'true';
+      wrapper.dataset.duplicated = "true";
     }
 
     // Calculate the width of the original content (not including clones)
-    const items = Array.from(wrapper.children).slice(0, wrapper.children.length / 2);
-    const originalContentWidth = items.reduce((acc, el) => acc + el.offsetWidth + parseInt(getComputedStyle(el).marginRight || 0), 0);
+    const items = Array.from(wrapper.children).slice(
+      0,
+      wrapper.children.length / 2
+    );
+    const originalContentWidth = items.reduce(
+      (acc, el) =>
+        acc + el.offsetWidth + parseInt(getComputedStyle(el).marginRight || 0),
+      0
+    );
 
     let x = 0;
-    const speed = 1; // px per frame
+    let speed = 1; // px per frame
+    const normalSpeed = 1;
+    const slowSpeed = 0.5;
+
+    // Add hover event listeners to slow down on hover
+    wrapper.addEventListener("mouseover", function (e) {
+      if (e.target.closest(".media-slider-item")) {
+        speed = slowSpeed;
+      }
+    });
+    wrapper.addEventListener("mouseout", function (e) {
+      if (e.target.closest(".media-slider-item")) {
+        speed = normalSpeed;
+      }
+    });
 
     function animate() {
       x -= speed;
@@ -756,16 +410,10 @@ $(document).ready(function () {
       .imagesLoaded()
       .progress({ background: true }, function (instance, image) {})
       .always(function (instance) {
-        sliderHero();
         tvSlider();
-        setHeightServieItems();
         scrollToService();
-        sliderHeroProjectDetail();
-        sliderProjects();
         sliderProjectsMobile();
-        sliderNews();
         backToTop();
-        handleGallerySlider();
         autoScrollMediaSlider();
         locoScroll.update();
         introATC();
